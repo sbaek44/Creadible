b5b6e850488859ad858adc9779c8026b1852c975
Sarah Baek <sbaek44@gmail.com>
2021-03-30 18:29:15 -0700

    rendered a random recipe from the api as the Recipe Of The Day and setup the ingredient input form

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,34 +1,82 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-import headers from '../../../config.js'
+import token from '../../../config.js'
 import Ingredients from './Ingredients.jsx'
 
 function App() {
   const [recipeOfTheDay, updateRecipeOfTheDay] = useState([])
 
   useEffect(() => {
-    getRecipeOfTheDay()
+    const storedRecipe = localStorage.getItem('recipeOfTheDay')
+    // storedRecipe ? updateRecipeOfTheDay(JSON.parse(storedRecipe)) : getRecipeOfTheDay()
+    storedRecipe ? updateRecipeOfTheDay(JSON.parse(storedRecipe)) : null
+  }, [])
+
+  useEffect(() => {
+    if (recipeOfTheDay.length > 0) {
+      let hours = 24
+      let now = new Date().getTime()
+      let setupTime = localStorage.getItem('setupTime')
+      if (setupTime === null) {
+        localStorage.setItem('setupTime', now)
+        localStorage.setItem('recipeOfTheDay', JSON.stringify(recipeOfTheDay))
+      } else {
+        if (now-setupTime > hours*60*60*1000) {
+          localStorage.clear()
+          localStorage.setItem('setupTime', now)
+        }
+      }
+    }
   }, [recipeOfTheDay])
 
   const options = {
     method: 'GET',
     url: 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random',
-    headers: headers
+    headers: {
+      'x-rapidapi-key': token,
+      'x-rapidapi-host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
+    }
   }
 
   const getRecipeOfTheDay = () => {
-    axios.get(options)
-    .then((results) => (updateRecipeOfTheDay(results.recipes)))
+    axios.request(options)
+    .then((results) => (updateRecipeOfTheDay(results.data.recipes)))
     .catch((err) => (console.log(err)))
   }
 
-  return (
-    <div>
-      <h1>Creadible!</h1>
-      <h3>Recipe Of The Day</h3>
-      <Ingredients />
-    </div>
-  )
+  if (recipeOfTheDay.length === 0) {
+    return (
+      <h1>Welcome To Creadible!</h1>
+    )
+  } else {
+    return (
+      <div>
+        <h1>Welcome To Creadible</h1>
+        <h3>Creating edible yummy dishes with the ingredients you already have!</h3>
+        <h4>Recipe Of The Day: <br />
+        {recipeOfTheDay[0].title}</h4>
+        <div id="recipe">
+          <img src={recipeOfTheDay[0].image}/>
+          <h5>Time: {recipeOfTheDay[0].readyInMinutes} minutes</h5>
+          <h5>Servings: {recipeOfTheDay[0].servings}</h5>
+          <h5>Ingredients</h5>
+          <ul>
+            {recipeOfTheDay[0].extendedIngredients.map((item, i) => (
+              <li key={i}>{item.originalString}</li>
+            ))}
+          </ul>
+          <h5>Directions</h5>
+          <ul>
+            {recipeOfTheDay[0].analyzedInstructions[0].steps.map((step, i) => (
+              <li key={i}>Step {step.number}: {step.step}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <Ingredients />
+      </div>
+    )
+  }
 }
 
 export default App
--- client/src/components/Ingredients.jsx
+++ client/src/components/Ingredients.jsx
@@ -1,31 +1,67 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import token from '../../../config.js'
 
 function Ingredients(props) {
   const [ingredients, updateIngredients] = useState([])
+  const [recipes, updateRecipes] = useState([])
 
-  // useEffect(() => {
-  //   getRecipes()
-  // }, [ingredients])
+  useEffect(() => {
+    // getRecipes()
+    console.log('ingredients', ingredients)
+  }, [ingredients])
 
-  // const getRecipes = () => {
-  //   axios.get()
-  // }
+  const options = {
+    method: 'GET',
+    url: 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients',
+    params: {
+      ingredients: ingredients,
+      number: 10,
+      ranking: 1,
+      ignorePantry: true
+    },
+    headers: {
+      'x-rapidapi-key': token,
+      'x-rapidapi-host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
+    }
+  }
 
-  // const handleChange = (e) => {
-  //   updateIngredients(value:)
-  // }
+  const handleChange = (e) => {
+    let input = [e.target.value]
+    updateIngredients(input.join(','))
+  }
 
-  return (
-    <div>
-      <form>
-        <label>
-          Ingredients on hand: <br />
-          <textarea type="text" name="ingredients"/>
-        </label>
-      </form>
-    </div>
-  )
+  const submitIngredients = (e) => {
+    e.preventDefault()
+    axios.request(options)
+    .then((results) => (updateRecipes(results.data)))
+    .then(() => ('recipes', console.log(recipes)))
+    .catch((err) => (console.log(err)))
+  }
+
+  if (recipes.length === 0) {
+    return (
+      <div>
+        <h2>What's For Dinner Tonight?</h2>
+        <h4>Type in your ingredients below and see all the Creadible dishes you can make!</h4>
+        <form onSubmit={submitIngredients}>
+          <label>
+            Ingredients on hand: <br />
+            <textarea onChange={handleChange} value={ingredients} type="text" name="ingredients"></textarea>
+          </label>
+          <button type="submit">Get Recipes</button>
+        </form>
+
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <h2>What's For Dinner Tonight?</h2>
+        <h4>Recipes using {ingredients}:</h4>
+      </div>
+    )
+  }
 }
 
 export default Ingredients
